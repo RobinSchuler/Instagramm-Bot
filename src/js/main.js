f5511dc84645625afcfe40c68ec1de87e88cf29b
Robin Schuler <robin.schuler@helloguest.de>
2018-04-19 11:55:12 +0200

    fix folllow and shorter duration

--- src/js/main.js
+++ src/js/main.js
@@ -25,7 +25,7 @@ class App extends React.Component {
         }, 2000);
       } else {
         elements[0].click();
-        var timeout = Math.random() * 180000 + 60000;
+        var timeout = Math.random() * 60000 + 10000;
         console.log("next in", timeout / 1000 / 60, "min");
         setTimeout(function() {
           clickElement(elements);
@@ -34,7 +34,7 @@ class App extends React.Component {
       // scrollToBotom(0);
     };
 
-    console.log("penis3");
+    console.log("start");
     var elements = document.getElementsByClassName(
       "_qv64e       _gexxb _4tgw8     _njrw0   "
     );
@@ -44,23 +44,32 @@ class App extends React.Component {
     //     await sleep(2000);
     // }
     clickElement(elements, 0);
-    return <div>Your App injected to DOM correctly!</div>;
+    return <div> </div>;
   }
 }
 
 // Message Listener function
 chrome.runtime.onMessage.addListener((request, sender, response) => {
+  console.log("runtimeMessage", request);
   // If message is injectApp
-  if (request.injectApp) {
+  if (request.follow) {
+    console.log("follow");
     // Inject our app to DOM and send response
-    injectApp();
+    follow();
+    response({
+      startedExtension: true
+    });
+  } else if (request.unfollow) {
+    console.log("unfollow");
+    unfollow();
     response({
       startedExtension: true
     });
   }
 });
 
-function injectApp() {
+function follow() {
+  console.log("func follow");
   const newDiv = document.createElement("div");
   newDiv.setAttribute("id", "chromeExtensionReactApp");
   document.body.appendChild(newDiv);
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,18 +1,37 @@
 window.onload = () => {
-    const $startButton = document.querySelector('.start');
-  
-    $startButton.onclick = () => {
-      // Get active tab
-      chrome.tabs.query({
+  const $followButton = document.querySelector(".follow");
+  const $unfollowButton = document.querySelector(".unfollow");
+
+  $followButton.onclick = () => {
+    console.log("follow clicked");
+    // Get active tab
+    chrome.tabs.query(
+      {
         active: true,
-        currentWindow: true,
-      }, (tabs) => {
+        currentWindow: true
+      },
+      tabs => {
         // Send message to script file
-        chrome.tabs.sendMessage(
-          tabs[0].id,
-          { injectApp: true },
-          response => window.close()
+        chrome.tabs.sendMessage(tabs[0].id, { follow: true }, response =>
+          window.close()
         );
-      });
-    };
-  }
+      }
+    );
+  };
+
+  $unfollowButton.onclick = () => {
+    // Get active tab
+    chrome.tabs.query(
+      {
+        active: true,
+        currentWindow: true
+      },
+      tabs => {
+        // Send message to script file
+        chrome.tabs.sendMessage(tabs[0].id, { unfollow: true }, response =>
+          window.close()
+        );
+      }
+    );
+  };
+};
